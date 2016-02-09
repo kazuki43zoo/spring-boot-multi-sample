1e1a866d62c70955a3e5ec41389fa2d03e7b432f
Kazuki Shimizu <kazuki43zoo@gmail.com>
2016-02-10 07:12:47 +0900

    Add WelcomeRestController

---
+++ sample-api/src/main/java/com/github/kazuki43zoo/sample/api/controller/WelcomeRestController.java
@@ -0,0 +1,18 @@
+package com.github.kazuki43zoo.sample.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Date;
+import java.util.Map;
+
+@RestController
+public class WelcomeRestController {
+
+    @RequestMapping("/")
+    public Map<String, Object> hello() {
+        return Collections.singletonMap("message", "Hello!!" + new Date());
+    }
+
+}
