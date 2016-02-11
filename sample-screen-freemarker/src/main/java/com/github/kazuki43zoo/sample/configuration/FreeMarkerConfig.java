7e63c17139df3b07716ac1e22aa28ce0361ead7e
Kazuki Shimizu <kazuki43zoo@gmail.com>
2016-02-11 21:43:05 +0900

    Prevent WARN log of FreeMarker

--- sample-screen-freemarker/src/main/java/com/github/kazuki43zoo/sample/configuration/FreeMarkerConfig.java
+++ sample-screen-freemarker/src/main/java/com/github/kazuki43zoo/sample/configuration/FreeMarkerConfig.java
@@ -1,6 +1,8 @@
 package com.github.kazuki43zoo.sample.configuration;
 
+import freemarker.ext.beans.BeansWrapperBuilder;
 import freemarker.ext.jsp.TaglibFactory;
+import freemarker.template.ObjectWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
@@ -14,8 +16,9 @@ public class FreeMarkerConfig {
     // TODO: This solutions is workaround to use JSP tag library on the Spring Boot executable jar.
     @Autowired
     void configureTaglibFactory(FreeMarkerConfigurer configurer) {
-        configurer.getTaglibFactory()
-                .setMetaInfTldSources(Arrays.asList(new TaglibFactory.ClasspathMetaInfTldSource(Pattern.compile(".*"))));
+        TaglibFactory factory = configurer.getTaglibFactory();
+        factory.setMetaInfTldSources(Arrays.asList(new TaglibFactory.ClasspathMetaInfTldSource(Pattern.compile(".*"))));
+        factory.setObjectWrapper(new BeansWrapperBuilder(freemarker.template.Configuration.VERSION_2_3_23).build());
     }
 
 }
