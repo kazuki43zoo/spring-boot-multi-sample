85f39ce2d8e79fb564b582ee134c727d17c532fc
Kazuki Shimizu <kazuki43zoo@gmail.com>
2016-02-21 13:01:16 +0900

    Fix build error in api-resource

--- database/src/main/java/com/github/kazuki43zoo/sample/configuration/WebMvcConfig.java
+++ database/src/main/java/com/github/kazuki43zoo/sample/configuration/WebMvcConfig.java
@@ -3,9 +3,11 @@ package com.github.kazuki43zoo.sample.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.h2.H2ConsoleProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@Profile("database")
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
