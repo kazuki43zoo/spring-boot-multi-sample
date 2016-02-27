bc111e83d9a961e7cfc1e95d7c84fbd589ee2bb9
Kazuki Shimizu <kazuki43zoo@gmail.com>
2016-02-27 16:32:03 +0900

    Change directory structures

--- release.sh
+++ release.sh
@@ -5,7 +5,6 @@ git pull
 
 # get artifact version
 VERSION=`mvn clean | grep 'Building.*Samples [0-9].*' | sed -e 's/^.*Building.*Samples //g'`
-CURRENT_DIR=`pwd`
 
 # stop services
 sudo service boot-api-c stop
@@ -24,35 +23,32 @@ if test ${buildResult} -ne 0 ; then
     exit ${buildResult}
 fi
 
-# copy artifacts to deploy dir
-chmod u+w services/*-${VERSION}.*
-cp database/target/database-${VERSION}.jar services/.
-cp screen-thymeleaf/target/screen-thymeleaf-${VERSION}.jar services/.
-cp screen-freemarker/target/screen-freemarker-${VERSION}.jar services/.
-cp screen-jsp/target/screen-jsp-${VERSION}.war services/.
-cp api-auth/target/api-auth-${VERSION}.jar services/.
-cp api-resource/target/api-resource-${VERSION}.jar services/.
-cp api-client/target/api-client-${VERSION}.jar services/.
+mkdir -p ${HOME}/apps/${VERSION}
 
-# change permission
-chmod 500 services/database-${VERSION}.jar
-chmod 500 services/screen-thymeleaf-${VERSION}.jar
-chmod 500 services/screen-freemarker-${VERSION}.jar
-chmod 500 services/screen-jsp-${VERSION}.war
-chmod 500 services/api-auth-${VERSION}.jar
-chmod 500 services/api-resource-${VERSION}.jar
-chmod 500 services/api-client-${VERSION}.jar
+# copy artifacts to deploy dir
+chmod u+w ${HOME}/apps/${VERSION}/*.*ar
+cp database/target/database-${VERSION}.jar ${HOME}/apps/${VERSION}/database.jar
+cp screen-thymeleaf/target/screen-thymeleaf-${VERSION}.jar ${HOME}/apps/${VERSION}/screen-thymeleaf.jar
+cp screen-freemarker/target/screen-freemarker-${VERSION}.jar ${HOME}/apps/${VERSION}/screen-freemarker.jar
+cp screen-jsp/target/screen-jsp-${VERSION}.war ${HOME}/apps/${VERSION}/screen-jsp.war
+cp api-auth/target/api-auth-${VERSION}.jar ${HOME}/apps/${VERSION}/api-auth.jar
+cp api-resource/target/api-resource-${VERSION}.jar ${HOME}/apps/${VERSION}/api-resource.jar
+cp api-client/target/api-client-${VERSION}.jar ${HOME}/apps/${VERSION}/api-client.jar
+chmod 500 ${HOME}/apps/${VERSION}/*.*ar
+
+# copy conf files to deploy dir
+cp service-config/*.conf {HOME}/apps/${VERSION}/.
 
 # register services
-sudo ln -f -s ${CURRENT_DIR}/services/database-${VERSION}.jar /etc/init.d/boot-db
-sudo ln -f -s ${CURRENT_DIR}/services/screen-thymeleaf-${VERSION}.jar /etc/init.d/boot-scr-t
-sudo ln -f -s ${CURRENT_DIR}/services/screen-freemarker-${VERSION}.jar /etc/init.d/boot-scr-f
-sudo ln -f -s ${CURRENT_DIR}/services/screen-jsp-${VERSION}.war /etc/init.d/boot-scr-j
-sudo ln -f -s ${CURRENT_DIR}/services/api-auth-${VERSION}.jar /etc/init.d/boot-api-a
-sudo ln -f -s ${CURRENT_DIR}/services/api-resource-${VERSION}.jar /etc/init.d/boot-api-r
-sudo ln -f -s ${CURRENT_DIR}/services/api-client-${VERSION}.jar /etc/init.d/boot-api-c
+sudo ln -f -s ${HOME}/apps/${VERSION}/database.jar /etc/init.d/boot-db
+sudo ln -f -s ${HOME}/apps/${VERSION}/screen-thymeleaf.jar /etc/init.d/boot-scr-t
+sudo ln -f -s ${HOME}/apps/${VERSION}/screen-freemarker.jar /etc/init.d/boot-scr-f
+sudo ln -f -s ${HOME}/apps/${VERSION}/screen-jsp.war /etc/init.d/boot-scr-j
+sudo ln -f -s ${HOME}/apps/${VERSION}/api-auth.jar /etc/init.d/boot-api-a
+sudo ln -f -s ${HOME}/apps/${VERSION}/api-resource.jar /etc/init.d/boot-api-r
+sudo ln -f -s ${HOME}/apps/${VERSION}/api-client.jar /etc/init.d/boot-api-c
 
-# setting auto-boot
+# delete auto-boot settings
 sudo chkconfig --del boot-db
 sudo chkconfig --del boot-scr-t
 sudo chkconfig --del boot-scr-f
@@ -61,7 +57,7 @@ sudo chkconfig --del boot-api-a
 sudo chkconfig --del boot-api-r
 sudo chkconfig --del boot-api-c
 
-# setting auto-boot
+# add auto-boot settings
 sudo chkconfig --add boot-db
 # sudo chkconfig --add boot-scr-t
 #sudo chkconfig --add boot-scr-f
