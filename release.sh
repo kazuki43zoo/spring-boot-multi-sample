a9b7a9b6395df5d89e47e35c733a42c0d7762451
Kazuki Shimizu <kazuki43zoo@gmail.com>
2016-02-27 11:40:52 +0900

    Add stop & start services

--- release.sh
+++ release.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# pull latest version
 git pull
 
 # get artifact version
@@ -13,6 +14,15 @@ if test ${buildResult} -ne 0 ; then
     echo "[ERROR] Failed a build."
     exit ${buildResult}
 fi
+
+# stop services
+sudo service boot-db boot-api-c stop
+sudo service boot-db boot-api-r stop
+sudo service boot-db boot-api-a stop
+sudo service boot-db boot-scr-j stop
+sudo service boot-db boot-scr-f stop
+sudo service boot-db boot-scr-t stop
+sudo service boot-db stop
 
 # copy artifacts to deploy dir
 chmod u+w services/*-${VERSION}.*
@@ -35,9 +45,18 @@ chmod 500 services/api-client-${VERSION}.jar
 
 # replace link
 sudo ln -f -s ${CURRENT_DIR}/services/database-${VERSION}.jar /etc/init.d/boot-db
-sudo ln -f -s ${CURRENT_DIR}/services/screen-thymeleaf-${VERSION}.jar /etc/init.d/boot-screen-t
-sudo ln -f -s ${CURRENT_DIR}/services/screen-freemarker-${VERSION}.jar /etc/init.d/boot-screen-f
-sudo ln -f -s ${CURRENT_DIR}/services/screen-jsp-${VERSION}.war /etc/init.d/boot-screen-j
+sudo ln -f -s ${CURRENT_DIR}/services/screen-thymeleaf-${VERSION}.jar /etc/init.d/boot-scr-t
+sudo ln -f -s ${CURRENT_DIR}/services/screen-freemarker-${VERSION}.jar /etc/init.d/boot-scr-f
+sudo ln -f -s ${CURRENT_DIR}/services/screen-jsp-${VERSION}.war /etc/init.d/boot-scr-j
 sudo ln -f -s ${CURRENT_DIR}/services/api-auth-${VERSION}.jar /etc/init.d/boot-api-a
 sudo ln -f -s ${CURRENT_DIR}/services/api-resource-${VERSION}.jar /etc/init.d/boot-api-r
 sudo ln -f -s ${CURRENT_DIR}/services/api-client-${VERSION}.jar /etc/init.d/boot-api-c
+
+# start services
+sudo service boot-db start
+sudo service boot-db boot-scr-t start
+sudo service boot-db boot-scr-f start
+sudo service boot-db boot-scr-j start
+sudo service boot-db boot-api-a start
+sudo service boot-db boot-api-r start
+sudo service boot-db boot-api-c start
