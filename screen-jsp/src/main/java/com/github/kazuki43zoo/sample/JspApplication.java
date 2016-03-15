e8b37b8ed46a97e87fad6dab8f01b86aff05193a
Kazuki Shimizu <kazuki43zoo@gmail.com>
2016-03-16 01:43:49 +0900

    Modify war -> jar at screen-jsrp

--- screen-jsp/src/main/java/com/github/kazuki43zoo/sample/JspApplication.java
+++ screen-jsp/src/main/java/com/github/kazuki43zoo/sample/JspApplication.java
@@ -2,15 +2,9 @@ package com.github.kazuki43zoo.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.resource.ResourceUrlEncodingFilter;
-
-import java.io.File;
 
 @ServletComponentScan
 @SpringBootApplication
@@ -18,24 +12,6 @@ public class JspApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JspApplication.class, args);
-    }
-
-    // TODO: This solutions is workaround for execution at working directory of multi project root.
-    @Profile("local")
-    @Component
-    private static class StandaloneEmbeddedServletContainerCustomizer implements EmbeddedServletContainerCustomizer {
-
-        @Override
-        public void customize(ConfigurableEmbeddedServletContainer container) {
-            File screen = new File("screen-jsp");
-            if (screen.exists()) {
-                File docRoot = new File(screen, "src/main/webapp");
-                if (docRoot.exists()) {
-                    container.setDocumentRoot(docRoot);
-                }
-            }
-        }
-
     }
 
     @Bean
