ce110ae30d0e9d6d9652503ff5e573e740275690
Kazuki Shimizu <kazuki43zoo@gmail.com>
2016-03-18 01:31:11 +0900

    Remove workaround for spring-boot#5125

--- screen-freemarker/src/main/java/com/github/kazuki43zoo/sample/FreeMarkerApplication.java
+++ screen-freemarker/src/main/java/com/github/kazuki43zoo/sample/FreeMarkerApplication.java
@@ -3,8 +3,6 @@ package com.github.kazuki43zoo.sample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.resource.ResourceUrlEncodingFilter;
 
 @ServletComponentScan
 @SpringBootApplication
@@ -12,12 +10,6 @@ public class FreeMarkerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FreeMarkerApplication.class, args);
-    }
-
-    // TODO: Workaround for https://github.com/spring-projects/spring-boot/issues/5125
-    @Bean
-    ResourceUrlEncodingFilter resourceUrlEncodingFilter() {
-        return new ResourceUrlEncodingFilter();
     }
 
 }
